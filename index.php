bb7b7d65cb3e5da44b7cb15e2ceb476c4da266c9
unknown <Marc@.kill2birds.com>
2011-02-11 23:24:06 -0500

    Pushing new layout

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html> 
+<html lang=en> 
+<head> 
+	<meta charset=UTF-8> 
+	<title>SOKOBAN</title> 
+	<!--[if lt IE 9]><script src=http://html5shiv.googlecode.com/svn/trunk/html5.js></script><![endif]--> 
+	<link href=css/styles.css rel=stylesheet /> 
+	<!--[if lt IE 9]><link href=css/ie.css rel=stylesheet /><![endif]--> 
+</head> 
+ 
+<body> 
+<header> 
+	<hgroup> 
+		<h1><a href=#>SOKOBAN</a></h1> 
+	</hgroup> 
+	<nav id=global> 
+		<ul> 
+			<li><a href=#>Home</a></li> 
+			<li><a href=#>Requests</a></li> 
+			<li id=services> 
+				<a href=#>Warehouse</a> 
+				<ul id=subMenu> 
+					<li><a href=#>Inventory</a></li> 
+					<li><a href=#>Move Order</a></li>  
+				</ul> 
+			</li> 
+			<li><a href=#>Items</a></li> 
+			<li><a href=#>Reports</a></li> 
+		</ul> 
+	</nav> 
+</header> 
+ 
+<div class=clearfix> 
+	<div id=content> 
+		<article> 
+			<header> 
+				<div class=time> 
+					<div class=year>2010</div> 
+					<div class=date>16<span>apr</span></div> 
+				</div> 
+				<h1>ACTIVE ORDERS</h1> 
+				<div class=comments>1</div> 
+			</header> 
+			<p>Insert Grid here</p> 
+			<footer> 
+				<em>Written by:</em> <strong>Marc Stroz</strong> 
+				<span class=newLine><em>Tags:</em> <a class=tags href=#>Dell</a><a class=tags href=#>Cisco</a><a class=tags href=#>network</a></span> 
+			</footer> 
+		</article> 
+	</div> 
+	
+	<aside> 
+		<section> 
+			<h1>Tracking Information</h1> 
+			<ul id=inTouch> 
+				<li><span class=twitter>12345</span> <a href=#>Current RFQ's</a></li> 
+				<li><span class=rss>4321</span> <a href=#>Items </a> in warehouse</li> 
+			</ul> 
+		</section> 
+		
+		<nav> 
+			<h1>Internal Forms</h1> 
+			<ul class=links> 
+				<li><a href=#>No Clue</a></li> 
+				<li><a href=#>No Clue 2</a></li> 
+				<li><a href=#>No Clue 3</a></li> 
+			</ul> 
+		</nav> 
+		
+		<section> 
+			<blockquote>It's only worth it if you buy it and deliver it</blockquote> 
+			<a class=twitterHandle href=#>@FGT323</a> 
+		</section> 
+	</aside> 
+</div> 
+ 
+<footer class=clearfix> 
+
+		<section> 
+			<h1>Active Orders</h1> 
+			<ul id=comments> 
+				<li> 
+					<a href=#>3 French Hens</a> 
+					<span class=newLine><strong>Comment Author</strong>, 20. 04. 2010.</span> 
+				</li> 
+				<li> 
+					<a href=#>2 Turtle Doves</a> 
+					<span class=newLine><strong>Comment Author</strong>, 5. 04. 2010.</span> 
+				</li> 
+				<li> 
+					<a href=#>Partridge in a pear tree</a> 
+					<span class=newLine><strong>Comment Author</strong>, 28. 03. 2010.</span> 
+				</li> 
+			</ul> 
+		</section> 
+		<section> 
+			<h1>Insert something Here</h1> 
+			
+		</section> 
+	<p class=copyright>Copyright &copy; 2011 <strong>SOKOBAN</strong>. All Rights Reserved.</p> 
+</footer> 
+</body> 
+</html>
